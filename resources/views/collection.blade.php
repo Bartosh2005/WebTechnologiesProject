79fafdc720d19e797a4a55d773e41301d5a46fbd
Hyperenor13 <serafeimkar1@gmail.co0m>
2025-10-01 14:14:53 +0200

    top page icon

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Game Vault</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('betterfavicon.ico') }}?v={{ time() }}">
 </head>
 <body>
      <header>
