c3d0f69ae866b0c097c5e24016b7714d7f3139aa
Bartosh2005 <bartorski2005@o2.pl>
2025-10-01 14:06:40 +0200

    adding grid

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -6,30 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/newsletter.css') }}">
 </head>
 <title>Image Grid</title>
-  <style>
-    .grid-container {
-      display: grid;
-      grid-template-columns: 1fr 2fr; /* image takes 1 part, text takes 2 */
-      gap: 20px; /* spacing */
-      max-width: 800px;
-      margin: auto;
-    }
 
-    .grid-item {
-      display: contents; /* lets children span the grid cells directly */
-    }
-
-    .grid-item img {
-      width: 100%;
-      border-radius: 8px;
-    }
-
-    .grid-item .text {
-      display: flex;
-      align-items: center; /* vertically center text */
-      font-size: 1.2rem;
-    }
-  </style>
 <body>
      <header>
         <a href="{{ url('/welcome') }}"><h1>LOGO</h1></a>
@@ -38,19 +15,53 @@
         <a href="{{ url('/collection') }}"><h1>MyCollection</h1></a>
     </header>
 
-        <div class="hero-image">
-                <div class="hero-text">
-                <h1>After 6 long years</h1>
-                <p>Silksong is finally out</p>
-            </div>
-        </div>
         
-        <div class="terminus-image">
-                <div class="terminus-text">
-                <h1>MarTak is rumored to be working on a new game</h1>
-                <p>It'll be awesome</p>
-            </div>
-        </div>
+
+<div class="grid-container">
+  <div class="featured-article">
+    <div class="overlay">
+      <h2>bla bla bla</h2>
+      <p>bla bla bla</p>
+    </div>
+  </div>
+
+  
+  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+    <div class="overlay">
+      <p>bla bla bla</p>
+    </div>
+  </div>
+
+  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+    <div class="overlay">
+      <p>bla bla bla</p>
+    </div>
+  </div>
+
+  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+    <div class="overlay">
+      <p>bla bla bla</p>
+    </div>
+  </div>
+
+  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+    <div class="overlay">
+      <p>bla bla bla</p>
+    </div>
+  </div>
+
+  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+    <div class="overlay">
+      <p>bla bla bla</p>
+    </div>
+  </div>
+
+  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+    <div class="overlay">
+      <p>bla bla bla</p>
+    </div>
+  </div>
+</div>
         
     <footer>
         <p>&copy; 2025 Game Library</p>
