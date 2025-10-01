21c9c0e25c4fff514fd2554a7aff733d5c72590c
MarcelFedorV <fedorovicimarcel@gmail.com>
2025-10-01 16:00:59 +0300

    V3 - added games and info

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -20,45 +20,45 @@
 <div class="grid-container">
   <div class="featured-article">
     <div class="overlay">
-      <h2>bla bla bla</h2>
-      <p>bla bla bla</p>
+      <h2>After 6 years - HollowKnight:SilkSong is finally here</h2>
+      <p>It's been a long wait, but gamers are excited to finally play the PEAK release</p>
     </div>
   </div>
 
   
-  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+  <div class="sub-article" style="background-image: url('/images/Hades2.jpg')">
     <div class="overlay">
-      <p>bla bla bla</p>
+      <p>Hades II - finally out of early access</p>
     </div>
   </div>
 
-  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+  <div class="sub-article" style="background-image: url('/images/ReTerminus.png')">
     <div class="overlay">
-      <p>bla bla bla</p>
+      <p>MarTek, the famous game company is rumored to be working on a new game</p>
     </div>
   </div>
 
-  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+  <div class="sub-article" style="background-image: url('/images/BorderLands2.png')">
     <div class="overlay">
-      <p>bla bla bla</p>
+      <p>BorderLands IV - Performance Issues, CEO says 'Get a better PC'</p>
     </div>
   </div>
 
-  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+  <div class="sub-article" style="background-image: url('/images/Ananta.png')">
     <div class="overlay">
-      <p>bla bla bla</p>
+      <p>Ananta - the anime-styled GTA game just got a new trailer</p>
     </div>
   </div>
 
-  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+  <div class="sub-article" style="background-image: url('/images/GOY.jpg')">
     <div class="overlay">
-      <p>bla bla bla</p>
+      <p>Ghost of Yotei - Masterpiece or Reused Content?</p>
     </div>
   </div>
 
-  <div class="sub-article" style="background-image: url('/images/silksong.jpg')">
+  <div class="sub-article" style="background-image: url('/images/SSG.jpeg')">
     <div class="overlay">
-      <p>bla bla bla</p>
+      <p>SilkSong: learn how to 'GIT GUD' with this new guide</p>
     </div>
   </div>
 </div>
