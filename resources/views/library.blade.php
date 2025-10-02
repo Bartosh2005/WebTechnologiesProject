ddd01546a40b0f8def79d6150b027ee2d355c5e8
jan0mann <jan.ferchof@icloud.com>
2025-10-02 09:24:22 +0200

    .

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -5,6 +5,7 @@
     <title>Game Vault</title>
     <link rel="stylesheet" href="{{ asset('css/library.css') }}">
     <link rel="stylesheet" href="{{ asset('css/libraryl.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('betterfavicon.ico') }}?v={{ time() }}">
 </head>
 
