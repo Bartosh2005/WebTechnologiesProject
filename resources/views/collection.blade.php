407a3f0e487b84d48339aaa931e8c5764d41f770
jan0mann <jan.ferchof@icloud.com>
2025-10-03 14:03:23 +0200

    headers similar

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -37,10 +37,11 @@
 </head>
 <body onload="loadSavedGames()">
      <header>
-        <a href="{{ url('/welcome') }}"><h1>LOGO</h1></a>
+        <a href="{{ url('/welcome') }}"><img src="{{ asset('image/Logo.png') }}" alt="Logo" style="width: 150px;"></a>
         <a href="{{ url('/library') }}"><h1>MyGamebrowser</h1></a>
         <a href="{{ url('/newsletter') }}"><h1>MyNewsletter</h1></a>
         <a href="{{ url('/collection') }}"><h1>MyCollection</h1></a>
+         <a href="{{ url('/account') }}"><button class="MyAccount-button">MyAccount</button></a>
     </header>
 
     <main>
--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -7,11 +7,12 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('betterfavicon.ico') }}?v={{ time() }}">
 </head>
 <body>
-     <header>
-        <a href="{{ url('/welcome') }}"><h1>LOGO</h1></a>
+    <header>
+        <a href="{{ url('/welcome') }}"><img src="{{ asset('image/Logo.png') }}" alt="Logo" style="width: 150px;"></a>
         <a href="{{ url('/library') }}"><h1>MyGamebrowser</h1></a>
         <a href="{{ url('/newsletter') }}"><h1>MyNewsletter</h1></a>
         <a href="{{ url('/collection') }}"><h1>MyCollection</h1></a>
+         <a href="{{ url('/account') }}"><button class="MyAccount-button">MyAccount</button></a>
     </header>
 
     <main>
