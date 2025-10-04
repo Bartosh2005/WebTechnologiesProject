b7056a3b7b5b56455bb0af7a6ab926eb9b6f3202
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2025-10-04 17:41:36 +0200

    changed icon for bigger one

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -5,7 +5,7 @@
     <title>Game Vault</title>
     <link rel="stylesheet" href="{{ asset('css/newsletter.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('Logo.ico') }}?v={{ time() }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('LogoJustIcon.ico') }}?v={{ time() }}">
 </head>
 <body>
     <header>
