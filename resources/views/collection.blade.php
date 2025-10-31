6bb3562168170cd13023be0f9504938d8f851a79
jan0mann <jan.ferchof@icloud.com>
2025-10-31 20:38:46 +0100

    Welcomepage edits. Gallery featured 4, and sliding gallery. Also included header file

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -44,13 +44,7 @@
     
 </head>
 <body onload="loadSavedGames()">
-     <header>
-        <a href="{{ url('/welcome') }}"><img src="{{ asset('image/Logo.png') }}" alt="Logo" style="width: 150px;"></a>
-        <a href="{{ url('/library') }}"><h1>MyGamebrowser</h1></a>
-        <a href="{{ url('/newsletter') }}"><h1>MyNewsletter</h1></a>
-        <a href="{{ url('/collection') }}"><h1>MyCollection</h1></a>
-         <a href="{{ url('/account') }}"><button class="MyAccount-button">MyAccount</button></a>
-    </header>
+    @include('layouts.header')
 
     <main>
         <div class="search" style="display:grid;grid-template-columns: 2% auto 2% 2%;padding:1%;">
