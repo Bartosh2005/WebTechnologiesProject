f3b27185d8a847f7c65f7f478a1ecbf61893b3eb
jan0mann <jan.ferchof@icloud.com>
2025-11-20 10:53:35 +0100

    redo changes that didnt work

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
@@ -14,7 +13,7 @@ class RegisterController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:12|regex:/[!@#$%^&*(),.?":{}|<>]/',
+            'password' => 'required|string|min:3',
         ]);
 
         $user = User::create([
