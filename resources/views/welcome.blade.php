70219a17be1080019ed01a58bfe68b2314f1b762
jan0mann <jan.ferchof@icloud.com>
2025-11-21 20:16:39 +0100

    redoing the home page cause of the new import of the games (hence now fetching the games)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,20 +11,34 @@
     @include('layouts.header')
 
     <main>
-    <section style="text-align: center; margin-top: 50px;">
-        <h2>Featured Games TODAY!</h2>
-        <div id="games-gallery" class="games-gallery">
-        </div>
-    </section>
+        <section style="text-align: center; margin-top: 50px;">
+            <h2>Featured Games TODAY!</h2>
 
-    <section class="sliding-gallery">
-        <h2>See what we can offer you!</h2>
-        <div id="sliding-gallery-container" class="sliding-gallery-container">
-        </div>
-    </section>
-    
-    <script src="{{ asset('js/gameslist.js') }}"></script>
-    <script src="{{ asset('js/welcome.js') }}"></script>
+            <div id="games-gallery" class="games-gallery">
+                @if(isset($randomGames) && $randomGames->count())
+                    @foreach($randomGames as $game)
+                        <div class="game-card">
+                            <img src="{{ $game->img }}" alt="{{ $game->title }}" loading="lazy">
+                            <div class="game-info">
+                                <h3 class="game-title">{{ $game->title }}</h3>
+                                @php
+                                    $text = $game->description ?? '';
+                                    $sentences = preg_split('/(?<=[.?!])\s+/', $text);
+                                    $limited = implode(' ', array_slice($sentences, 0, 2));
+                                @endphp
+                                <p class="game-description">{{ $limited }}</p>
+                                @if(!empty($game->company))
+                                    <p class="game-company">{{ $game->company }}</p>
+                                @endif
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <p>No games available yet.</p>
+                @endif
+            </div>
+        </section>
+
     </main>
 
     <footer>
