460e98a79064158e8d2a0c19cf22b8ac057dc52d
Bartosh2005 <bartorski2005@o2.pl>
2025-11-23 10:20:55 +0100

    myaccount button fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,9 @@
     <a href="{{ url('/library') }}"><h1>MyGamebrowser</h1></a>
     <a href="{{ url('/newsletter') }}"><h1>MyNewsletter</h1></a>
     <a href="{{ url('/collection') }}"><h1>MyCollection</h1></a>
-    <a href="{{ route('myaccount') }}"><button class="MyAccount-button">MyAccount</button></a>
+    @if(auth()->check())
+        <a href="{{ route('myaccount') }}"><button class="MyAccount-button">MyAccount</button></a>
+    @endif
     @if(auth()->check() || session('role') === 'admin')
         @if(session('role') === 'admin')
             <span class="adminMessage">
